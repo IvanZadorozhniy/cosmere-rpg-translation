d618152325b1697e21adef1ade3a12429edf5a67
Ivan Zadorozhnyi <i.zadorozniy@yahoo.com>
2025-01-09 14:48:52 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+{
+			"lang":"ru",
+			"path": "i18n/systems/cosmere-rpg.json",
+			"system": "cosmere-rpg"
+		}
---
+++ esm/cosmere-rpg-playtest.js
@@ -0,0 +1,7 @@
+import { s as t } from "./shared.js";
+function o() {
+  t("cosmere-rpg-playtest");
+}
+export {
+  o as init
+};
